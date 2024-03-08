9b596a64b64f6e08910f492bad2d2bc4b6bbd31b
Casper Lam <casper.lam.kh@googlemail.com>
2024-03-08 16:35:13 +0000

    add video details jsx

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import videos from "./data/video-details.json";
 import Header from "./components/Header/Header.jsx";
 import Video from "./components/Video/Video.jsx";
 import VideoList from "./components/VideoList/VideoList.jsx";
+import Description from "./components/Description/Description.jsx";
 
 export default function App() {
   const [selectedVideo, setSelectedvideo] = useState(videos[0]);
@@ -20,7 +21,10 @@ export default function App() {
       <Header />
       <main className="main">
         <Video video={selectedVideo} />
-        <VideoList videos={list} handleVideoClick={handleVideoClick} />
+        <section className="main__content">
+          <Description video={selectedVideo} />
+          <VideoList videos={list} handleVideoClick={handleVideoClick} />
+        </section>
       </main>
     </>
   );
---
+++ src/components/Description/Description.jsx
@@ -0,0 +1,30 @@
+import "./Description.scss";
+import viewsIcon from "../../assets/icons/views.svg";
+import likesIcon from "../../assets/icons/likes.svg";
+
+export default function Description({ video }) {
+  return (
+    <section className="description">
+      <h2 className="description__title">{video.title}</h2>
+      <section className="description__statWrapper">
+        <wrapper className="description__left">
+          <p className="description__channel">By {video.channel}</p>
+          <p className="description__date">
+            {new Date(video.timestamp).toLocaleDateString()}
+          </p>
+        </wrapper>
+        <wrapper className="description__right">
+          <div className="description__views">
+            <img src={viewsIcon} alt="Views icon" />
+            <p className="description__viewsNumber">{video.views}</p>
+          </div>
+          <div className="description__likes">
+            <img src={likesIcon} alt="Views icon" />
+            <p className="description__likesNumber">{video.likes}</p>
+          </div>
+        </wrapper>
+      </section>
+      <p className="description__text">{video.description}</p>
+    </section>
+  );
+}
