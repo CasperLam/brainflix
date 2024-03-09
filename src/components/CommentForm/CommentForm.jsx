56d564eeea60d1057a2bef1a6edf59fbc7d0997a
Casper Lam <casper.lam.kh@googlemail.com>
2024-03-09 14:47:51 +0000

    add responsive styling to comment form

--- src/components/CommentForm/CommentForm.jsx
+++ src/components/CommentForm/CommentForm.jsx
@@ -1,21 +1,24 @@
+import Button from "../Button/Button";
 import "./CommentForm.scss";
 
 export default function CommentForm() {
   return (
     <>
       <form className="form">
-        <div className="form__group">
+        <div className="form__wrapper">
           <label htmlFor="comment" className="form__label">
             JOIN THE CONVERSATION
           </label>
-          <textarea
-            name="comment"
-            id="comment"
-            className="form__input form__input--area"
-            placeholder="Add a new comment"
-          ></textarea>
+          <div className="form__content">
+            <textarea
+              name="comment"
+              id="comment"
+              className="form__input"
+              placeholder="Add a new comment"
+            ></textarea>
+            <Button className="button button--form">COMMENT</Button>
+          </div>
         </div>
-        <button className="form__btn">COMMENT</button>
       </form>
     </>
   );
