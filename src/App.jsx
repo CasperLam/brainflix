5a9b710f98f05892d68c8cb23401903408c081df
Casper Lam <casper.lam.kh@googlemail.com>
2024-03-11 16:25:52 +0000

    refactor app and homepage files for new folder structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,13 @@
-import { useState } from "react";
 import "./App.scss";
-import videos from "./data/video-details.json";
 import Header from "./components/Header/Header.jsx";
-import Video from "./components/Video/Video.jsx";
-import VideoList from "./components/VideoList/VideoList.jsx";
-import Description from "./components/Description/Description.jsx";
-import CommentSection from "./components/CommentSection/CommentSection.jsx";
+import Homepage from "./pages/Homepage/Homepage.jsx";
 
 export default function App() {
-  const [selectedVideo, setSelectedvideo] = useState(videos[0]);
-
-  const handleVideoClick = (video) => {
-    setSelectedvideo(video);
-  };
-
-  const list = videos.filter((video) => {
-    return video !== selectedVideo;
-  });
   return (
     <>
       <Header />
       <main className="main">
-        <Video video={selectedVideo} />
-        <section className="main__content">
-          <section className="main__commentSection">
-            <Description video={selectedVideo} />
-            <CommentSection video={selectedVideo} />
-          </section>
-          <VideoList videos={list} handleVideoClick={handleVideoClick} />
-        </section>
+        <Homepage />
       </main>
     </>
   );
---
+++ src/pages/Homepage/Homepage.jsx
@@ -0,0 +1,31 @@
+import { useState } from "react";
+import videos from "../../data/video-details.json";
+import "./Homepage.scss";
+import Video from "../../components/Video/Video";
+import Description from "../../components/Description/Description";
+import CommentSection from "../../components/CommentSection/CommentSection";
+import VideoList from "../../components/VideoList/VideoList";
+
+export default function Homepage() {
+  const [selectedVideo, setSelectedvideo] = useState(videos[0]);
+
+  const handleVideoClick = (video) => {
+    setSelectedvideo(video);
+  };
+
+  const list = videos.filter((video) => {
+    return video !== selectedVideo;
+  });
+  return (
+    <>
+      <Video video={selectedVideo} />
+      <section className="main__content">
+        <section className="main__commentSection">
+          <Description video={selectedVideo} />
+          <CommentSection video={selectedVideo} />
+        </section>
+        <VideoList videos={list} handleVideoClick={handleVideoClick} />
+      </section>
+    </>
+  );
+}
