7cb6baa9779fbcfdc0d8d23f573756fb32ba1a90
Casper Lam <casper.lam.kh@googlemail.com>
2024-03-13 12:09:46 +0000

    add header route links

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,19 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import "./App.scss";
 import Header from "./components/Header/Header.jsx";
-// import Homepage from "./pages/Homepage/Homepage.jsx";
+import Homepage from "./pages/Homepage/Homepage.jsx";
 import Upload from "./pages/Upload/Upload.jsx";
 
 export default function App() {
   return (
     <>
-      <Header />
-
-      {/* <Homepage /> */}
-      <Upload />
+      <BrowserRouter>
+        <Header />
+        <Routes>
+          <Route path="/" element={<Homepage />} />
+          <Route path="/upload" element={<Upload />} />
+        </Routes>
+      </BrowserRouter>
     </>
   );
 }
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -3,12 +3,15 @@ import Search from "../SearchBar/Search";
 import logoImg from "../../assets/logo/BrainFlix-logo.svg";
 import avatar from "../../assets/images/Mohan-muruge.jpg";
 import Button from "../Button/Button";
+import { Link } from "react-router-dom";
 
 export default function Header() {
   const logo = logoImg;
   return (
     <header className="header">
-      <img src={logo} alt="BrainFlix logo" className="header__logo" />
+      <Link to="/" className="header__link">
+        <img src={logo} alt="BrainFlix logo" className="header__logo" />
+      </Link>
       <section className="header__search">
         <Search />
         <img
@@ -17,7 +20,9 @@ export default function Header() {
           className="header__avatar header__avatar--mobile"
         />
       </section>
-      <Button className="button button--header">UPLOAD</Button>
+      <Link to="/upload">
+        <Button className="button button--header">UPLOAD</Button>
+      </Link>
       <img
         src={avatar}
         alt="User's avatar"
